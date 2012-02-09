57e21863a250328a35d4324ff801eeb1755f1748
Anselm Helbig <anselm.helbig+github@googlemail.com>
2012-02-09 21:00:49 +0100

    rename rainbows.rb to rainbows-config.rb; enabled production settings

--- rainbows-config.rb
+++ rainbows-config.rb
@@ -1,14 +1,13 @@
-# -*- mode: ruby -*-
-
 Rainbows! do
   use :ThreadPool # concurrency model to use
   worker_connections 400
   keepalive_timeout 0 # zero disables keepalives entirely
-  client_max_body_size 5*1024*1024 # 5 megabytes
+  client_max_body_size 512*1024*1024 # 512 megabytes
   keepalive_requests 666 # default:100
   client_header_buffer_size 2 * 1024 # 2 kilobytes
 end
 
 worker_processes 2
-# stderr_path "log/error.log"
-# stdout_path "log/output.log"
+stderr_path "log/error.log"
+stdout_path "log/output.log"
+pid "log/rainbows.pid"
